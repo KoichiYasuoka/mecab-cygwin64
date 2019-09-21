c36ef309a3000ac8b5139e0434db7c3da5eb63e6
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-09-21 11:51:12 +0900

    ipadic added

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/sh -x
 INSTALL_DIR=${1-/usr/local}
-for D in bin etc include lib libexec/mecab share/man/man1
+for D in bin etc include lib libexec/mecab share/man/man1 lib/mecab/dic/ipadic
 do install -d $INSTALL_DIR/$D
    install $D/* $INSTALL_DIR/$D
 done
