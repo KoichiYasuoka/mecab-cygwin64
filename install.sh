b28ec4810c551c16fbf292c911869a3fdbdd08d7
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-09-21 12:26:14 +0900

    bug fix for installing mecab-config

--- install.sh
+++ install.sh
@@ -4,4 +4,7 @@ for D in bin etc include lib libexec/mecab share/man/man1 lib/mecab/dic/ipadic
 do install -d $INSTALL_DIR/$D
    install $D/* $INSTALL_DIR/$D
 done
+if [ $INSTALL_DIR != /usr/local ]
+then sed 's?/usr/local?'$INSTALL_DIR'?' bin/mecab-config > $INSTALL_DIR/bin/mecab-config
+fi
 exit 0
