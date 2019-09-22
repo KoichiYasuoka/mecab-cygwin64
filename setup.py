3add3671324c7b3f742ed005e895d9da5dbe65ee
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-09-22 15:04:02 +0900

    bug fix

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ else:
 
 setuptools.setup(
   name="mecab-cygwin64",
-  version="0.996.1",
+  version="0.996.2",
   packages=setuptools.find_packages(),
   data_files=[
     ("local/bin",glob.glob("bin/*")),
     ("local/libexec/mecab",glob.glob("libexec/mecab/*")),
     ("local/lib",glob.glob("lib/*.*")),
+    ("local/etc",glob.glob("etc/*")),
     ("local/include",glob.glob("include/*")),
     ("local/share/man/man1",glob.glob("share/man/man1/*")),
     ("local/lib/mecab/dic/ipadic",glob.glob("lib/mecab/dic/ipadic/*"))
